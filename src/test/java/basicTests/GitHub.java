d5bb0e1cc845abcb8382a0fcbefaa0a74cf7af2e
Avinashkr03 <avinash030092@gmail.com>
2019-06-12 21:47:56 +0530

    second

--- src/test/java/basicTests/GitHub.java
+++ src/test/java/basicTests/GitHub.java
@@ -11,5 +11,6 @@ public void repository() {
 	System.setProperty("webdriver.gecko.driver", "C:/geckodriver-v0.24.0-win64/geckodriver.exe");
 	WebDriver driver = new FirefoxDriver();	
 driver.get("https://www.amazon.com/");
+System.out.println("fdj");
 }
 }
